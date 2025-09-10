e00c3afca60fde07978e70069403a476921c7d0b
edwardmartin1 <edwardmartin1@yahoo.com>
2025-09-09 23:19:03 -0500

    commit unfinished code

---
+++ cart.js
@@ -0,0 +1,125 @@
+
+function addItem(cart = [], item = "", quantity = 0)
+{
+    try
+    {
+        if (!Array.isArray(cart))
+        {
+            throw new Error("Cart must be an array");
+        }
+        
+        if (typeof item !== "string")
+        {
+            throw new Error("Item must be a string.");
+        }
+
+        if (!item)
+        {
+            throw new Error("No item was provided.");
+        }
+
+        if (item.trim() === "")
+        {
+            throw new Error("Blank item was provided.");
+        }
+    
+        if (typeof quantity !== "number")
+        {
+            throw new Error("Quantity must be a number.");
+        }
+
+        if (quantity < 1)
+        {
+            throw new Error("Quantity must be great than 0.");
+        }
+    
+        let newItem =
+        {
+            item: item,
+            quantity: quantity
+        };
+
+        cart.push(newItem);
+
+        return cart;
+    }
+
+    catch (err)
+    {
+        console.error(`Error: ${err.message}`);
+
+        return cart;
+    }
+
+}
+
+function removeItem(cart, item)
+{
+    try
+    {
+        if (!Array.isArray(cart))
+        {
+            throw new Error("Cart must be an array");
+        }
+
+        if ((cart.find((objectName) => objectName.item === item)) === undefined)
+        {
+            throw new Error("Item not found in cart");
+        }
+
+        cart.splice(cart.findIndex((objectName) => objectName.item === item), 1);
+
+        return cart;
+
+    }
+
+    catch (err)
+    {
+        console.error(`Error: ${err.message}`);
+
+        return cart;
+    }
+
+}
+
+function getTotalItems(cart)
+{
+    try
+    {
+        if (!Array.isArray(cart))
+        {
+            throw new Error("Cart must be an array");
+        }
+
+        return cart.reduce((total, objectName) => total + objectName.quantity, 0);    
+    }
+
+    catch (err)
+    {
+        console.error(`Error: ${err.message}`);
+
+        return 0;
+    }       
+}
+
+
+let cart = [];
+
+cart = addItem(cart, "peanut butter", 5);
+cart = addItem(cart, "pizza", 15);
+cart = addItem(cart, "chocolate", 16);
+console.log(cart);
+console.log();
+console.log(removeItem(cart, "pizza"));
+console.log();
+console.log(getTotalItems(cart));
+
+/*
+module.exports =
+{
+    addItem: addItem//,
+    //removeItem: removeItem,
+    //getTotalItems: getTotalItems
+};
+
+*/
